32aa3a50276770c3fd15c8b8a17c61d9089670b0
Sarthak <sarthakr748@gmail.com>
2023-06-10 22:44:49 +0530

    added

---
+++ SetMatrixZeroes.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	// Write your code here.
+    int n = matrix.size();
+        int m = matrix[0].size();
+        bool firstRow = false;  // do we need to set first row zero
+        bool firstCol = false;  // do we need to ser first col zero
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+					if(matrix[i][j] == 0){   // store rows and cols state in first row and col
+                    if(i==0) firstRow = true;
+                    if(j==0) firstCol = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        // cout<<firstRow<<" "<<firstCol<<endl;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if(firstRow){
+            for(int i=0;i<m;i++) matrix[0][i] = 0;
+        }
+        
+        if(firstCol){
+            for(int i=0;i<n;i++) matrix[i][0] = 0;
+        }
+}
