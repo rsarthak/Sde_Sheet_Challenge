0b1f8f819ed61529e1bc1fa9c241be6ad5a03573
Sarthak Rajput <60438849+rsarthak@users.noreply.github.com>
2023-06-12 22:36:00 +0530

    Create MergeTwoSortedArrays.cpp

---
+++ MergeTwoSortedArrays.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	// Write your code here.
+	for(int i=0;i<n;i++)
+	arr1.pop_back();
+	for(int i=0;i<n;i++)arr1.push_back(arr2[i]);
+	sort(arr1.begin(),arr1.end());
+	return arr1;
+}
